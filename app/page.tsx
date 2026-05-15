a0c69ffe205b112c4abbf714951795dea3da9e9c
hariharan-developer-31 <hariharansankar4@gmail.com>
2026-05-15 11:28:03 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Hello Developers... 🚀
+      </h1>
+    </main>
+  );
+}
